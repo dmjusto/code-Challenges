a2399e2da54d3dab65bdf39f86dde56692c48b3d
Dylan Fairbanks <31903571+dmjusto@users.noreply.github.com>
2019-03-10 16:36:25 -0600

    Add files via upload

---
+++ Factorial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Factorial
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int factorial = GetFactorialRecursive(4);
+      //int factorial = GetFactorialItterative(4);
+      Console.WriteLine(factorial);
+    }
+
+    public static int GetFactorialRecursive(int n)
+    {
+      if (n -1 == 0)
+      {
+        return n;
+      }
+      n *= GetFactorialRecursive(n-1);
+      return n;
+    }
+
+    public static int GetFactorialItterative(int n)
+    {
+      for(int i = n -1; i > 0; i--)
+      {
+        n *= i;
+      }
+      return n;
+    }
+  }
+}
---
+++ Fibonacci.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fibonacci
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int result = FibonacciRecursive(7);
+      Console.WriteLine(result);
+
+    }
+
+
+    public static int FibonacciRecursive(int n)
+    {
+      if(n < 2)
+      {
+        return n;
+      }
+      int next = FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
+      return next;
+      //each number is the sum of the two before it
+      //n =(n-1) + (n-2)
+    }
+
+    public static int FibonacciItterative(int index)
+    {
+      int current = 1;
+      int previous = 0;
+      int temp;
+
+      for (int i = 1; i < index; i++)
+      {
+        Console.WriteLine(current);
+        temp = current;
+        current += previous;
+        previous = temp;
+      }
+
+      return current;
+    }
+    
+  }
+}
+
+
+//[0,1,1,2,3,5,8,13]
